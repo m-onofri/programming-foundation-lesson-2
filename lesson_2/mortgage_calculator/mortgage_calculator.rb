4ec752fd702a40a380d1d09360c7cd8d6503d715
Manuele Onofri <manuele.onofri@gmail.com>
2016-10-14 22:30:13 +0200

    Modify mortgage_calculator.rb

--- lesson_2/mortgage_calculator/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -1,118 +1,131 @@
 # Methods
 
-# This method return true when num has ONE or TWO digits max after the point
-def float?(num)
-  /^\d*\.\d{1,2}$/.match(num)
+def prompt(message)
+  puts "=>> #{message}"
 end
 
-# This method return true when num is formed by ONE or MORE digits
-def integer?(num)
-  /^\d+$/.match(num)
-end
-
-
-# Program
-
-puts "Welcome to the Mortgage Calculator!"
-
-puts "Please, enter your name:"
-user_name = ""
-loop do
-  user_name = gets.chomp
-
-  if user_name.empty?
-    "You must enter a valid name"
-  else
-    break
-  end
-end
-
-puts "Hi #{user_name}. To calculate your montly payment, we need to know some information."
-
-loop do # ùin this loop I perform the calculation for the montly payment
-    loan_amount   = ''
-    interest_rate = ''
-    duration_unit = ''
-    duration_loan = ''
-
-  loop do # In this loop I collect the information to calculate the montly payment
-    
-    puts "What's the loan amount?"
-    loop do
-      loan_amount = gets.chomp
-
-      # loan_amount must be:
-      # a float number (that is 100.00) or an integer number (that is 100)
-      # greater than zero
-      if float?(loan_amount) || integer?(loan_amount)
-        if loan_amount.to_f > 0
-          break
-        else
-          puts "The loan amount can't be zero or lower. Please, enter a valid loan amount:"
-        end
-      else
-        puts "The loan amount is not valid. Enter a valid loan amount"
-      end
-    end
-
-    puts "What is the interest rate?\n(Example: 5 for 5% or 2.5 for 2.5%)"
-    loop do
-      interest_rate = gets.chomp
-
-      # interest_rate must be:
-      # a float number (that is 2.23) or an integer number
-      # greater than zero
-      if float?(interest_rate) || integer?(interest_rate)
-        if interest_rate.to_f > 0
-          break
-        else
-          puts "The interest rate can't be zero or lower. Please, enter a valid interest rate:"
-        end
-      else
-        puts "The interest rate is not valid. Enter a valid interest rate"
-      end
-    end
-
-    puts "Now we need to know the duration of the loan; please choose between this two option: years or months"
-    
-    loop do
-      duration_unit = gets.chomp
-
-      if duration_unit.downcase.start_with('y') || duration_unit.downcase.start_with('m')
-        break
-      else
-        puts "You must choose between years or months. Please enter your choice:"
-      end
-    end
-
-    puts "What is the duration of the loan (in #{duration_unit})?"
-    loop do
-      duration_loan = gets.chomp
-
-      # duration_loan must be:
-      # a float number (that is 1.5 years) or an integer number
-      # greater than zero
-      if float?(duration_loan)
-        if duration_loan.to_f > 0
-          break
-        else
-          puts "The loan duration can't be zero or lower. Please, enter a valid loan duration:"
-        end
-      else
-        puts "The loan duration is not valid. Enter a valid loan duration"
-      end
-    end
-
-    puts "Loan amount: #{loan_amount} €\ninterest rate: #{interest_rate}%\nLoan duration: #{duration_loan} #{duration_unit}"
-    puts "Do you confirm these information? (enter yes to conferm)"
-    answer1 = gets.chomp
-    if answer1 == "yes"
+def ask_name
+  prompt "Please, enter your name:"
+  user_name = ""
+  loop do
+    user_name = gets.chomp
+    if user_name.empty?
+      puts "You must enter a valid name:"
+    else
       break
     end
   end
+  user_name
+end
 
-  interest_per_year = interest_rate.to_f / 100
-  interest_per_month = interest_per_year / 12
+def float? string
+  /\d/.match(string) && /^\d*\.?\d*$/.match(string)
+end
+
+def integer? string
+  /^\d+$/.match(string)
+end
+
+def valid_number? string
+  float?(string) || integer?(string)
+end
+
+def ask_info info, request
+  prompt request
+  value = ''
+  loop do
+    value = gets.chomp
+    if valid_number? value
+      if value.to_f > 0
+        break
+      else
+        puts "The #{info} can't be zero or lower. Please, enter a valid #{info}:"
+      end
+    else
+      puts "The #{info} is not valid. Enter a valid #{info}:"
+    end
+  end
+  value
+end
+
+def set_duration_unit
+  prompt "Will you be entering the duration of your loan in months or years? (months/years)")
+  duration_unit = ''
+  loop do
+    duration_unit = gets.chomp
+    if duration_unit.downcase == "years" || duration_unit.downcase == "months"
+      break
+    else
+      puts "You must choose between years or months. Please enter your choice:"
+    end
+  end
+  duration_unit
+end
+
+def correct_answer 
+  answer = ''
+  loop do
+    answer = gets.chomp
+    if answer.downcase == "yes" || answer.downcase == "no"
+      break
+    else
+      puts "Your answer is not correct; please enter yes or no:"
+    end
+  end
+  answer
+end
+
+def loan_amount_format string
+  if string.include?(".")
+    positive = string.split(".")[0].split(//)
+    negative = string.split(".")[1]
+  else
+    positive = string.split(//)
+    negative = ''
+  end
+  output_array = [] 
+  i = 1
+  while !positive.empty?
+    if i % 4 == 0
+      output_array.unshift(",")
+      i = 1
+    else
+      output_array.unshift(positive.last)
+      positive.pop
+      i += 1
+    end
+  end
+  result = output_array.join + "." + negative
+end
+
+# Program
+puts "Welcome to the Mortgage Calculator!"
+
+user_name = ask_name
+
+puts "Hi #{user_name}. To calculate your montly payment, we need to know some information."
+
+loop do 
+  loan_amount   = ''
+  interest_rate = ''
+  duration_unit = ''
+  duration_loan = ''
+
+  loop do 
+    loan_amount = ask_info "loan amount", "Please enter your loan amount (in euro):"
+    interest_rate = ask_info "interest rate", "Please enter your interest rate (example: 2.5 for 2.5%)"
+    duration_unit = set_duration_unit
+    duration_loan = ask_info "loan duration", "Please enter your loan duration, in number of #{duration_unit}:"
+
+    puts "Loan amount: #{loan_amount_format(format('%02.2f', loan_amount))}€\ninterest rate: #{format('%02.2f', interest_rate)}%\nLoan duration: #{format('%02.1f', duration_loan)} #{duration_unit}"
+    puts "Do you confirm these information? (yes/no)"
+    answer1 = correct_answer
+    if answer1 == "yes"
+      break
+    end
+  end # End inner loop
+
+  interest_per_month = interest_rate.to_f / 100 / 12
 
   if duration_unit == "years"
     total_months = duration_loan.to_f * 12
@@ -120,21 +133,25 @@ loop do # ùin this loop I perform the calculation for the montly payment
     total_months = duration_loan.to_f
   end
 
-  montly_payment = (loan_amount.to_f * (interest_per_month / (1 - (1 + interest_per_month)**-total_months))).round(2)
+  montly_payment = loan_amount.to_f * (interest_per_month / (1 - (1 + interest_per_month)**-total_months))
 
-  puts "Your monthly payment is: #{montly_payment} €"
+  puts "Your monthly payment is: #{format('%02.2f', montly_payment)}€"
 
-  puts "Would you like to perform another calculation? (enter yes to conferm)"
-  answer2 = gets.chomp
+  puts "Would you like to perform another calculation? (yes/no)"
+  answer2 = correct_answer
+  if answer2 == "no"
+    break
+  end
+end # End outer loop
 
-  break unless answer2 == "yes"
-end
-
-puts "Thank you for using the Mortgage Calculator!"
+puts "Thank you #{user_name} for using the Mortgage Calculator!"
 puts "Good bye!"
 
-# Write method for check input value
-# Put the strings in YAML file
+#Refactor the resume part
+#Eliminate the user_name part?
+
+
+
 
 
 
