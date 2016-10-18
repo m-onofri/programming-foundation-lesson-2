a7e38e9f3c279353fcdb8acece9cc0bf8ae77395
Manuele Onofri <manuele.onofri@gmail.com>
2016-10-18 22:09:07 +0200

    Complete walk-through rock paper scissors

--- lesson_2/README.md
+++
@@ -1,1 +0,0 @@
-# programming-foundation-lesson-2
---
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -0,0 +1,47 @@
+VALID_CHOICES = %w(rock paper scissors)
+
+def display_result(player, computer)
+  if win?(player, computer)
+    "You won!"
+  elsif win?(computer, player)
+    "Computer won!"
+  else
+    "It's a tie!"
+  end
+end
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt "That'snot a valid choice."
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt "You chose: #{choice}; Conputer chose: #{computer_choice}"
+
+  prompt display_result(choice, computer_choice)
+
+  prompt "Do you want to play again?"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thank you for playing. Good bye!"
